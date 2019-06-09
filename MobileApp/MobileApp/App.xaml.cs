9d86c1173bd84c4dafabf8518e736fb28c38d31c
Amr Swalha <amr.swalha.code@outlook.com>
2019-06-09 22:34:17 +0400

    Add project files.

---
+++ MobileApp/MobileApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileApp.Services;
+using MobileApp.Views;
+using System.Diagnostics;
+
+namespace MobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+            Debug.WriteLine("My App is Started");
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            Current.Properties["username"] = "Joe";
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            var username = Current.Properties["username"];
+            // Handle when your app resumes
+        }
+    }
+}
