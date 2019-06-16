6d20b01ffb7537d4de7406c167058044039fbc30
Amr Swalha <amr.swalha.code@outlook.com>
2019-06-16 22:05:46 +0400

    Notify Property + DatePicker

--- MobileApp/MobileApp/ViewModels/ButtonViewModel.cs
+++ MobileApp/MobileApp/ViewModels/ButtonViewModel.cs
@@ -1,21 +1,35 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MobileApp.ViewModels
 {
-    public class ButtonViewModel
+    public class ButtonViewModel : BaseViewModel
     {
         public ButtonViewModel(string lblText)
         {
             Title = "Button Page";
             LblText = lblText;
-            ButtonCommand = new Command(() => {
+            ButtonCommand = new Command(() =>
+            {
                 Debug.WriteLine("WriteLine from command");
+            });
+            NotifyCommand = new Command(() =>
+            {
+                NotifyTxt = "Value is updated";
             });
         }
         public string Title { get; set; }
         public string LblText { get; set; }
+        private string notifyTxt = "Initial Value";
+        public string NotifyTxt
+        {
+            get { return notifyTxt; }
+            set { SetProperty(ref notifyTxt, value); }
+        }
         public ICommand ButtonCommand { get; set; }
+        public ICommand NotifyCommand { get; set; }
+
     }
 }
---
+++ MobileApp/MobileApp/Views/ControlsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControlsPage : ContentPage
+    {
+        public ControlsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DatepickerCtrl_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            Debug.WriteLine(datepickerCtrl.Date);
+        }
+    }
+}
