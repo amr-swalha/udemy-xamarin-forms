dbf79703ebc9ee3aff978e0bbc6bc6feeb2c9128
Amr Swalha <amr.swalha.code@outlook.com>
2019-06-19 21:42:27 +0400

    Alerts and Images

---
+++ MobileApp/MobileApp/Views/AlertsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertsPage : ContentPage
+    {
+        public AlertsPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Message_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("The Title", "Hello World!", "Ok");
+        }
+        private async void YesNo_Clicked(object sender, EventArgs e)
+        {
+            var result = await  DisplayAlert("The Title", "Hello World!", "Ok","Cancel");
+            Debug.WriteLine(result);
+        }
+    }
+}
