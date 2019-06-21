77d548734093d035780ec885fcd00d2a5d555f05
Amr Swalha <amr.swalha.code@outlook.com>
2019-06-21 17:32:53 +0400

    Picker

---
+++ MobileApp/MobileApp/Views/PickerPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerPage : ContentPage
+    {
+        List<Order> Orders = new List<Order>();
+        public PickerPage()
+        {
+            InitializeComponent();
+            Orders.Add(new Order { CustomerName = "Joe", Amount = 100 });
+            Orders.Add(new Order { CustomerName = "David", Amount = 300 });
+            BindingContext = Orders;
+        }
+
+        private void OrdersList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var order = (Order)((Picker)sender).SelectedItem;
+            Debug.WriteLine(order.Amount);
+        }
+    }
+}
