bd01b739c6e6b66224a04086cf7dda0793d0fd4c
Amr Swalha <amr.swalha.code@outlook.com>
2019-06-22 13:45:06 +0400

    Xamarin Essentials

---
+++ MobileApp/MobileApp/Views/EssentialsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EssentialsPage : ContentPage
+    {
+        public EssentialsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var level = Battery.ChargeLevel;
+        }
+
+        private void ShareBtn_Clicked(object sender, EventArgs e)
+        {
+            Share.RequestAsync(new ShareTextRequest { Text = "Share Link", Uri = "https://google.com" });
+        }
+    }
+}
